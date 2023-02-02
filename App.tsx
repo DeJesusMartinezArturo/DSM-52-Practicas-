90f9e7608e9b634a546ba9ab242716307c3882b9
arturo <oxz810@gmail.com>
2023-02-02 08:34:24 -0600

    Agregamos un parrafo

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}
+        <Text>Rama Daniela </Text>
       </Text>
       <Text
         style={[
